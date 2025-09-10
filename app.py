e3f08e3589ed50e84b71f2b21383e8eb13ee4d93
Anxietye <alejandrog_10_01@hotmail.com>
2025-09-10 16:26:57 -0400

    Descuento ISA

--- app.py
+++ app.py
@@ -1370,6 +1370,7 @@ VALID_COUPONS = {
     "SAVEBIG": {"type": "percent", "value": 25},  # 25% de descuento
     "TAKE10": {"type": "percent", "value": 10},  # $10 de descuento fijo
     "LAUNCH40": {"type": "percent", "value": 40},  # $40 de descuento fijo
+    "YANE100": {"type": "percent", "value": 100},  # 100 de descuento fijo
 }
 COUPONS_ENABLED = True
 
